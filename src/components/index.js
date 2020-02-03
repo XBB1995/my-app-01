39f1d1f239e97bc90d6228a324ac6ebaac10b7f8
XBB <358118276@qq.com>
2020-02-03 20:10:20 +0800

    learn git dev push

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,2 @@
-<<<<<<< HEAD
 export { default as Counter } from './Counter'
 export { default as CountBtn } from './CountBtn'
-=======
-// import TodoHeader from "./TodoHeader"
-// import TodoInput from "./TodoInput"
-// import TodoList from "./TodoList"
-
-// export { TodoHeader, TodoInput, TodoList }
-
-export { default as TodoHeader } from './TodoHeader'
-export { default as TodoInput } from './TodoInput'
-export { default as TodoList } from './TodoList'
-export { default as Like } from './Like'
->>>>>>> 534797f39523a917ca70c5dfc36073abd536679c
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './App'
-<<<<<<< HEAD
 import { CounterProvider } from './counterStore'
 
 render(
   <CounterProvider>
     <App />
   </CounterProvider>,
-=======
-
-render(
-  <App />,
->>>>>>> 534797f39523a917ca70c5dfc36073abd536679c
   document.querySelector('#root')
 )
